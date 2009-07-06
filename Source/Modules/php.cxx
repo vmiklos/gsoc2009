fbe19173f285bcc77fb6d1d775cce11ee81d450a
vmiklos <vmiklos@626c5289-ae23-0410-ae9c-e8d60b6d4f22>
2009-07-06 17:53:59 +0000

    Fix -prefix when returning from a %newobject method
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2009-vmiklos@11373 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Source/Modules/php.cxx
+++ Source/Modules/php.cxx
@@ -1600,7 +1600,7 @@ public:
 	     * _p_Foo -> Foo, _p_ns__Bar -> Bar
 	     * TODO: do this in a more elegant way
 	     */
-	    Printf(output, "\t\tif (is_resource($r)) $class=substr(get_resource_type($r), (strpos(get_resource_type($r), \"__\") ? strpos(get_resource_type($r), \"__\") + 2 : 3));\n");
+	    Printf(output, "\t\tif (is_resource($r)) $class='%s'.substr(get_resource_type($r), (strpos(get_resource_type($r), \"__\") ? strpos(get_resource_type($r), \"__\") + 2 : 3));\n", prefix);
 	    Printf(output, "\t\treturn is_resource($r) ? new $class($r) : $r;\n");
 	  } else {
 	    Printf(output, "\t\t$this->%s = $r;\n", SWIG_PTR);
