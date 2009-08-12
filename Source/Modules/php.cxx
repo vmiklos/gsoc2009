vmiklos/gsoc2009

commit e92c638507eb04325a49048e61526be00f49f372

PHP: fix for the overload_rename testcase
Previously we just had two __construct function which resulted in a
parse error. Now we create a new factory method for the renamed ctor.

git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@11510 626c5289-ae23-0410-ae9c-e8d60b6d4f22

Source/Modules/php.cxx
@@ -1026,7 +1026,13 @@ public:
       String *output = s_oowrappers;
       if (constructor) {
 	class_has_ctor = true;
-	methodname = "__construct";
+	if (strcmp(GetChar(n, "name"), GetChar(n, "constructorHandler:sym:name")) == 0) {
+	  methodname = "__construct";
+	} else {
+	  // The class has multiple constructors and this one is
+	  // renamed, so this will be a static factory function
+	  methodname = GetChar(n, "constructorHandler:sym:name");
+	}
       } else if (wrapperType == memberfn) {
 	methodname = Char(Getattr(n, "memberfunctionHandler:sym:name"));
       } else if (wrapperType == staticmemberfn) {
@@ -1585,7 +1591,11 @@ public:
 	Printf(output, "%s", prepare);
       if (constructor) {
 	if (!directorsEnabled() || !Swig_directorclass(n)) {
-	  Printf(output, "\t\t$this->%s=%s;\n", SWIG_PTR, invoke);
+	  if (strcmp(methodname, "__construct") == 0) {
+	    Printf(output, "\t\t$this->%s=%s;\n", SWIG_PTR, invoke);
+	  } else {
+	    Printf(output, "\t\treturn new %s(%s);\n", "Foo", invoke);
+	  }
 	} else {
 	  Node *parent = Swig_methodclass(n);
 	  String *classname = Swig_class_name(parent);

Examples/test-suite/php/overload_rename_runme.php
@@ -1,0 +1,19 @@
+<?php
+
+require "tests.php";
+require "overload_rename.php";
+
+// No new functions
+check::functions(array());
+// No new classes
+check::classes(array(Foo));
+// now new vars
+check::globals(array());
+
+$f = new Foo(1.0);
+$f = new Foo(1.0,1.0);
+$f = Foo::Foo_int(1.0,1);
+$f = Foo::Foo_int(1.0,1,1.0);
+
+check::done();
+?>

commit 3ba58bdb41a7ac8f5bdedc3b3be63d9a666cd6e6

PHP: fix for the extend_template testcase
The fix is not to handle a %template as a rename by reading the class'
sym:name attribute instead of the ctor's name attribute.

This has been broken in r11510.

git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@11539 626c5289-ae23-0410-ae9c-e8d60b6d4f22

Source/Modules/php.cxx
@@ -1027,11 +1027,11 @@ public:
       if (constructor) {
 	class_has_ctor = true;
 	// Skip the Foo:: prefix.
-	char *ptr = strrchr(GetChar(n, "name"), ':');
+	char *ptr = strrchr(GetChar(Swig_methodclass(n), "sym:name"), ':');
 	if (ptr) {
 	  ptr++;
 	} else {
-	  ptr = GetChar(n, "name");
+	  ptr = GetChar(Swig_methodclass(n), "sym:name");
 	}
 	if (strcmp(ptr, GetChar(n, "constructorHandler:sym:name")) == 0) {
 	  methodname = "__construct";